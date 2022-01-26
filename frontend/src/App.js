05d078bf119d08364b6d8d10b75db2dda18b0c1e
Kyle Ortega <kyleortega253@gmail.com>
2022-01-25 22:17:39 -0800

    creating space layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import nasaService from './services/nasaService'
+import logo from './images/earth.JPG'
+import Image from 'react-bootstrap/Image'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  useEffect(() => {
-    async function getPic() {
-      console.log(process.env.APP_KEY)
-      const data = await nasaService.getPicture()
-      console.log(data)
-    }
+  const [image, changeImage] = useState('')
 
-    getPic();
-  }, [])
+  // useEffect(() => {
+  //   async function getPic() {
+  //     console.log(process.env.APP_KEY )
+  //     const response = await nasaService.getPicture()
+  //     console.log(response.data)
+  //   }
+
+  //   getPic();
+  // }, [])
 
   return (
     <div className='App'>
       <Navigation />
-      <p>stuff</p>
+      <p>Astronomy Picture of the Day</p>
+      <img 
+        src={logo}
+        height='100'
+      />
     </div>
   );
 }
