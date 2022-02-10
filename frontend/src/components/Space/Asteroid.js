5b784a16739b198556e246ad8622d7dee88c9c60
Kyle Ortega <kyleortega253@gmail.com>
2022-02-09 17:17:52 -0800

    tabled created

--- frontend/src/components/Space/Asteroid.js
+++ frontend/src/components/Space/Asteroid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'react-bootstrap';
 import './Asteroid.css';
 
@@ -15,9 +15,14 @@ const Asteroid = ({ stroid }) => {
         changeEnd('')
     }
 
-    // console.log('stroid', stroid)
+    console.log('stroid', stroid)
+    // for (const key in stroid) {
+    //     console.log(stroid[key])
+    // }
+    console.log(Object.values(stroid))
     return (
-        <div>
+        <>
+            <h2>Near Earth Asteroids</h2>
             <form onSubmit={submitForm}>
                 <label>
                     Start Date: 
@@ -39,7 +44,31 @@ const Asteroid = ({ stroid }) => {
                 </label>
                 <button className='button'>Submit</button>
             </form>
-        </div>
+            {/* <Table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Miss Distance</th>
+                        <th>Velocity</th>
+                        <th>Min. Diameter</th>
+                        <th>Max Diameter</th>
+                        <th>Hazardous</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.values(stroid).map(el => (
+                        <tr key={el.neo_reference_id}>
+                            <td>{el.close_approach_data[0].close_approach_date_full}</td>
+                            <td>{el.close_approach_data[0].miss_distance.kilometers} km</td>
+                            <td>{el.close_approach_data[0].relative_velocity.kilometers_per_hour} km/hr</td>
+                            <td>{el.estimated_diameter.kilometers.estimated_diameter_min} km</td>
+                            <td>{el.estimated_diameter.kilometers.estimated_diameter_max} km</td>
+                            <td>{el.is_potentially_hazardous_asteroid}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table> */}
+        </>
     )
 }
 
