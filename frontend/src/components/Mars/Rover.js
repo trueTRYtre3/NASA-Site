e59e693bab5eedac75cfcae2e87444f963bff742
Kyle Ortega <kyleortega253@gmail.com>
2022-02-16 00:36:30 -0800

    rewrote rover

--- frontend/src/components/Mars/Rover.js
+++ frontend/src/components/Mars/Rover.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import Scroller from '../InfiniteScroll/Scroller';
-// import { Spinner } from 'react-bootstrap';
-// import InfiniteScroll from 'react-infinite-scroll-component';
+import { Spinner } from 'react-bootstrap';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import '../InfiniteScroll/Scroller.css';
 import './Rover.css';
 
 const Rover = ({ pictures }) => {
@@ -14,44 +14,34 @@ const Rover = ({ pictures }) => {
 
     const changeDataLength = () => {
         const newLength = limit+12 < photos.length ? limit+12 : photos.length  
-        // .slice()
         setTimeout(() => {
             changeLimit(newLength)
-        }, 1500)
+        }, 1000)
     }
 
     return (
-        <>
+        <div className='grid-outer-container'>
             {photos && 
-            <Scroller 
-                limit={limit} 
-                changeFn={changeDataLength}
-                len={photos.length}
-                mappedObj={photos}
-            />}
-        </>
-        // <div className='mars-page'>
-        //     {photos && 
-        //     <div className='pictures'>
-        //         <InfiniteScroll
-        //             dataLength={limit}
-        //             next={changeDataLength}
-        //             hasMore={limit < photos.length}
-        //             loader={ 
-        //                 <div>
-        //                     <br />
-        //                     <Spinner animation="grow" variant="light" className='spin' />
-        //                 </div>
-        //             }
-        //             endMessage={<h1 style={{ color: 'white' }}>That's all folks</h1>}
-        //         >
-        //             {photos.slice(0, limit).map(pics => (
-        //                 <img key={pics.id} src={pics.img_src} loading='lazy' alt='failed load' width='300' height='300' />
-        //             ))}
-        //         </InfiniteScroll>
-        //     </div>
-        //     }
-        // </div>
+            <div className='grid-inner'>
+                <InfiniteScroll
+                    dataLength={limit}
+                    next={changeDataLength}
+                    hasMore={limit < photos.length}
+                    loader={ 
+                        <div>
+                            <br />
+                            <Spinner animation="grow" variant="light" className='spin' />
+                        </div>
+                    }
+                    endMessage={<h1 style={{ color: 'white' }}>That's all folks</h1>}
+                >
+                    {photos.slice(0, limit).map(pics => (
+                        <img key={pics.id} src={pics.img_src} loading='lazy' alt='failed load' width='300' height='300' />
+                    ))}
+                </InfiniteScroll>
+            </div>
+            }
+        </div>
     )
 }
 
